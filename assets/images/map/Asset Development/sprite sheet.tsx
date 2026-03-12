b8ac729bd060e5bd73b3bee404a65d351cc48da0
RileyDoesGameDev <Rileyartstuff12@gmail.com>
2026-03-12 11:05:28 -0600

    adding assets

---
+++ assets/images/map/Asset Development/sprite sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sprite sheet" tilewidth="64" tileheight="64" tilecount="162" columns="18">
+ <image source="sprite sheet.png" width="1152" height="576"/>
+</tileset>
